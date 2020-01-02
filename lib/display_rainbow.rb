bbf76de351b57bbde7826f0feadbd593a26c2e20
Miles Higbie <miles.higbie@gmail.com>
2020-01-02 16:11:02 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,10 @@
 # def display_rainbow(red, orange, yellow, green, blue, indigo, violet)
 # end
 
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
 def display_rainbow(colors)
+  colors.each do |color|
+    puts color[0][0] 
+  end 
 end 
