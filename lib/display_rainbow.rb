26c0080853421dd6bb57371797581b37aab1f1f4
Miles Higbie <miles.higbie@gmail.com>
2020-01-02 16:16:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,9 +5,7 @@
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 def display_rainbow(colors)
-  new_arr = []
   colors.each do |color|
-    new_arr.push("#{color[0][0].upcase}: #{color}")
+    puts ("#{color[0][0].upcase}: #{color}")
   end 
-  new_arr.join(",")
 end 
